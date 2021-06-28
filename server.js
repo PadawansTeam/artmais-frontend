30afbdd388b5712b0a2026e13f6fd5ea73d200f2
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 18:27:19 -0300

    test

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const nomeApp = process.env.npm_package_name;
 const app = express();
 
 //Serve only the static files form the dist directory
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}/src`));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/src/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
