c644ae3aa30ab52eabbfd267aee40043e92666ac
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-27 22:38:42 -0300

    Login request Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,34 +15,33 @@ import { TesteModule } from './teste/teste.module';
 import { HeaderModule } from './header/header.module';
 import { FooterModule } from './footer/footer.module';
 
+import { LoginService } from './login/service/login.service';
+import { CadastroService } from './cadastro/service/cadastro.service';
+
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
       loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-  }),
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     LoginModule,
     CadastroModule,
     TesteModule,
     HeaderModule,
-    FooterModule
+    FooterModule,
   ],
-  providers: [
-    TranslateService
-  ],
-  bootstrap: [AppComponent]
+  providers: [TranslateService, LoginService, CadastroService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/component/login.component.ts
+++ src/app/login/component/login.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoginService } from '../service/login.service';
+import { LoginDto, LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  constructor(
-    private loginService: LoginService,
-    private router: Router
-  ) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public login() {
+    let teste = {
+      email: 'joao.teixeira@aluno.ifsp.edu.br',
+      password: 'padawans#2021',
+    } as LoginDto;
+    this.loginService.authenticate(teste).subscribe(
+      (response) => {
+        console.log(response);
+        return response;
+      },
+      (err) => {
+        throw err;
+      }
+    );
   }
-
-  // this.router.navigate(['/teste']);
-  
-
 }
