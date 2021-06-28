606cd4d91adf0af71ba64c198ebe351655ebd602
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 16:30:36 -0300

    trying new path to boot

--- serve.js
+++ serve.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/artmais-frontend-heroku/src/"));
+app.use(express.static("./dist/artmais-frontend/src/"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/artmais-frontend-heroku/src/" })
+  res.sendFile("index.html", { root: "dist/artmais-frontend/src/" })
 );
 
 // Start the app by listening on the default Heroku port
