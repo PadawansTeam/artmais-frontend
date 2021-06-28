1637babecbdb1e8571c8fc23ebe82c8e3fe997fd
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 15:46:06 -0300

    Try to use ngIf

--- src/app/select/component/select.component.ts
+++ src/app/select/component/select.component.ts
@@ -3,22 +3,19 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'select-lang',
-  templateUrl: './select.component.html'
+  templateUrl: './select.component.html',
 })
 export class SelectLangComponent implements OnInit {
-
   constructor(public translate: TranslateService) {
     this.translate.setDefaultLang('pt-BR');
     translate.addLangs(['pt-BR', 'en-US']);
     this.translate.use(localStorage.getItem('lang') || 'pt-BR');
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   switchLang(lang: string): void {
     localStorage.setItem('lang', lang);
     window.location.reload();
   }
-
 }
