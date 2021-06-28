c954b497100c76d709a37a6851d2ebd294a11b21
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 18:12:32 -0300

    new path server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 //Serve only the static files form the dist directory
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/dist/artmais-frontend/src/`));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/src/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/artmais-frontend/src/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
