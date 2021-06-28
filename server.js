6853cc972d432998a8850693a3b90741a1b750ff
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 18:20:20 -0300

    change package and server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
 //Serve only the static files form the dist directory
-app.use(express.static(`${__dirname}/dist/artmais-frontend/src/`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/artmais-frontend/src/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
