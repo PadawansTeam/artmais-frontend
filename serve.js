b4a464b9eb423aebab1dcb418554466aa25ae4d0
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 16:19:07 -0300

    change directory server.js

--- serve.js
+++ serve.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/artmais-frontend-heroku"));
+app.use(express.static("./dist/artmais-frontend-heroku/src/"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/artmais-frontend-heroku/" })
+  res.sendFile("index.html", { root: "dist/artmais-frontend-heroku/src/" })
 );
 
 // Start the app by listening on the default Heroku port
