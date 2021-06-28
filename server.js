67fe6b2c083ae00bd2137a6330fb0601b1737175
juliana-santiago <juju_msantiago@hotmail.com>
2021-06-28 17:05:14 -0300

    try new directory

--- server.js
+++ server.js
@@ -40,18 +40,12 @@ const app = express();
 //   })
 // );
 
-// Serve only the static files form the dist directory
-// app.use(express.static(`${__dirname}/dist/artmais-frontend/src/`));
+//Serve only the static files form the dist directory
+app.use(express.static(`${__dirname}/dist/artmais-frontend/src/`));
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(`${__dirname}/dist/artmais-frontend/src/index.html`));
-// });
-// Serve only the static files form the dist directory
-app.use(express.static("./dist/artmais-frontend/src/"));
-
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/artmais-frontend/src/" })
-);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/artmais-frontend/src/index.html`));
+});
 
 // Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 4200;
