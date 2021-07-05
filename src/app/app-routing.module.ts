2036ebc95ec7fda01850b796660a7310dbd110f6
Junior Torres <juniortorres.mtj@gmail.com>
2021-07-05 00:00:53 -0300

    Add new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/component/cadastro.component';
+import { ConstrucaoComponent } from './construcao/component/construcao.component';
+import { ErroComponent } from './erro/component/erro.component';
+import { HomepageComponent } from './homepage/component/homepage.component';
 import { InicioComponent } from './inicio/component/inicio.component';
 import { LoginComponent } from './login/component/login.component';
+import { PlanosComponent } from './planos/componet/planos.component';
 import { TesteComponent } from './teste/component/teste.component';
+
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: 'teste', component: TesteComponent }
+  { path: 'teste', component: TesteComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'erro', component: ErroComponent },
+  { path: 'construcao', component: ConstrucaoComponent },
+  { path: 'planos', component: PlanosComponent },
+  { path: 'homepage', component: HomepageComponent },
 ];
 
 @NgModule({
---
+++ src/app/homepage/homepage.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { SelectModule } from '../select/select.module';
+import { HeaderModule } from '../header/header.module';
+import { FooterModule } from '../footer/footer.module';
+import { HomepageComponent } from './component/homepage.component';
+
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
+
+
+@NgModule({
+  declarations: [
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  }),
+    SelectModule,
+    HeaderModule,
+    FooterModule
+  ],
+  providers: [
+    TranslateService
+  ],
+  bootstrap: []
+})
+export class HomepageModule { }
