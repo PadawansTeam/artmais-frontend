aaf54f0e2408728f9a74285d3a51ca74f4d37c82
Junior Torres <juniortorres.mtj@gmail.com>
2021-07-06 00:33:23 -0300

    Add new component

--- src/app/headerlog/headerlog.module.ts
+++ src/app/headerlog/headerlog.module.ts
@@ -4,20 +4,19 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SelectModule } from '../select/select.module';
-import { HeaderModule } from '../header/header.module';
-import { FooterModule } from '../footer/footer.module';
-import { HomepageComponent } from './component/homepage.component';
-import { HeaderlogModule } from '../headerlog/headerlog.module';
+import { HeaderlogComponent } from './component/headerlog.component';
 
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
 }
 
-
 @NgModule({
   declarations: [
-    HomepageComponent
+    HeaderlogComponent,
+  ],
+  exports: [
+    HeaderlogComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,13 +27,11 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
           deps: [HttpClient]
       }
   }),
-    SelectModule,
-    HeaderlogModule,
-    FooterModule
+    SelectModule
   ],
   providers: [
     TranslateService
   ],
   bootstrap: []
 })
-export class HomepageModule { }
+export class HeaderlogModule { }
