8cf3d8c6c5e48543e009f16e823beffd453eccdc
Junior Torres <juniortorres.mtj@gmail.com>
2021-07-15 22:29:48 -0300

    Remove comments and fix cadastro css

--- src/assets/js/material-kit.js
+++ src/assets/js/material-kit.js
@@ -1,26 +1,9 @@
-/*!
-
- =========================================================
- * Material Kit - v2.0.7
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/material-kit
- * Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
- * Designed by www.invisionapp.com Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
 
 var big_image;
 
 $(document).ready(function() {
   BrowserDetect.init();
 
-  // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
   $('body').bootstrapMaterialDesign();
 
   window_width = $(window).width();
