10188956899bd506cada1cf917761634ae8f8a8c
Gabriela Viana Bilinski <gabriela.bilinski@bilinski.com.br>
2021-07-22 21:31:38 -0300

    feat(removed-mocks): removed mocks

--- src/app/homepage/component/homepage.component.ts
+++ src/app/homepage/component/homepage.component.ts
@@ -10,7 +10,6 @@ import { RecommendationService } from '../service/recommendation.service';
 export class HomepageComponent implements OnInit {
 
   recommendations: Recommendation[] = [];
-  teste: Recommendation[] = [{username: 'Glória Groove', backgroundPicture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNADvHxU_Mw0nEyJnQ3olDb-KTE9nbVLMhvQ&usqp=CAU", userPicture: "https://images.pexels.com/photos/209948/pexels-photo-209948.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", category: 'Dança', subcategory: 'Moderna'}, {username: 'Glória Groove', backgroundPicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNADvHxU_Mw0nEyJnQ3olDb-KTE9nbVLMhvQ&usqp=CAU", userPicture: "https://images.pexels.com/photos/209948/pexels-photo-209948.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", category: 'Dança', subcategory: 'Moderna'}, {username: 'Glória Groove', backgroundPicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNADvHxU_Mw0nEyJnQ3olDb-KTE9nbVLMhvQ&usqp=CAU", userPicture: "https://images.pexels.com/photos/209948/pexels-photo-209948.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", category: 'Tatuagem', subcategory: 'Pontilhismo'}];
 
   constructor(
     public recommendationService: RecommendationService
