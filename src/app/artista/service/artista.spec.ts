6613602271ab7f96c33d572e8009bc1b73bcbcdf
Gabriela Viana Bilinski <gabriela.bilinski@bilinski.com.br>
2021-07-25 20:54:58 -0300

    feat(pagina-perfil): integracao da pagina de perfil

---
+++ src/app/artista/service/artista.spec.ts
@@ -0,0 +1,7 @@
+import { Artista } from './artista';
+
+describe('Artista', () => {
+  it('should create an instance', () => {
+    expect(new Artista()).toBeTruthy();
+  });
+});
---
+++ src/app/perfil/service/perfil.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PerfilService {
+  public artPlusURL = `${environment.apiURL}`;
+  public token = localStorage.getItem('token')
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getUser(): Observable<any>{
+    return this.http.get(this.artPlusURL + 'v1/User', this.httpOptions)
+  }
+}
