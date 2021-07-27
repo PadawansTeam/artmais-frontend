5f6db9a3b123cd4a1abea084f78221d868a4eb94
Gabriela Viana Bilinski <gabriela.bilinski@bilinski.com.br>
2021-07-26 21:35:15 -0300

    feat(integracao-configuracao): integracao configuracao

---
+++ src/app/configuracao/service/configuracao.service.ts
@@ -0,0 +1,100 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfiguracaoService {
+  public artPlusURL = `${environment.apiURL}`;
+  public token = localStorage.getItem('token')
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getUserInfo(): Observable<any>{
+    return this.http.get(this.artPlusURL + 'v1/User', this.httpOptions)
+  }
+
+  getContactInfo(): Observable<any>{
+    return this.http.get(this.artPlusURL + 'v1/Contact', this.httpOptions)
+  }
+
+  getAddress(): Observable<any>{
+    return this.http.get(this.artPlusURL + 'v1/Address', this.httpOptions)
+  }
+
+  updateUserInfo(
+    name: string,
+    username: string,
+    userPicture: string,
+    birthDate: Date,
+    mainPhone: string,
+    secundaryPhone: string,
+    thirdPhone: string
+  ): Observable<object> {
+    return this.http.put<object>(
+      `${this.artPlusURL}` + 'v1/User/UpdateUserInfo',
+      { name, username, userPicture, birthDate, mainPhone, secundaryPhone, thirdPhone },
+      this.httpOptions
+    );
+  }
+
+  updatePassword(
+    oldPassword: string,
+    oldPasswordConfirmation: string,
+    password: string
+  ): Observable<object> {
+    return this.http.patch<object>(
+      `${this.artPlusURL}` + 'v1/User/UpdateUserPassword',
+      { oldPassword, oldPasswordConfirmation, password },
+      this.httpOptions
+    );
+  }
+
+  updateDescription(
+    description: string,
+  ): Observable<object> {
+    return this.http.patch<object>(
+      `${this.artPlusURL}` + 'v1/User/UpdateUserDescription',
+      { description },
+      this.httpOptions
+    );
+  }
+
+  updateContact(
+    facebook: string,
+    instagram: string,
+    twitter: string,
+    mainPhone: string,
+    secundaryPhone: string,
+    thirdPhone: string,
+  ): Observable<object> {
+    return this.http.post<object>(
+      `${this.artPlusURL}` + 'v1/Contact/Upsert',
+      { facebook, instagram, twitter, mainPhone, secundaryPhone, thirdPhone },
+      this.httpOptions
+    );
+  }
+
+  updateAddress(
+    street: string,
+    number: number,
+    complement: string,
+    neighborhood: string,
+    zipCode:string
+  ): Observable<object> {
+    return this.http.post<object>(
+      `${this.artPlusURL}` + 'v1/Address/Upsert',
+      { street, number, complement, neighborhood, zipCode },
+      this.httpOptions
+    );
+  }
+}
