4cc1e66cd3019a3e838b4a13a27b13f149668629
juliana-santiago <juju_msantiago@hotmail.com>
2021-08-08 23:22:23 -0300

    Add Components and Services Portfolio

--- src/app/artista/component/artista.component.ts
+++ src/app/artista/component/artista.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Artista } from '../service/artista';
 import { ArtistaService } from '../service/artista.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Portfolio } from '../service/Portfolio';
 
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html',
-  styleUrls: ['./artista.component.css']
+  styleUrls: ['./artista.component.css'],
 })
 export class ArtistaComponent implements OnInit {
-
   artist!: Artista;
   idUser!: number;
+  portfolioImages: Portfolio[] = [];
 
   constructor(
     public artistaService: ArtistaService,
@@ -23,13 +24,21 @@ export class ArtistaComponent implements OnInit {
     this.idUser = this.route.snapshot.params['id'];
     this.artistaService.getArtista(this.idUser).subscribe(
       (response: Artista) => {
-        console.log(response)
+        console.log(response);
         this.artist = response;
       },
       (err) => {
         throw err;
       }
-    )
+    ),
+      this.artistaService.getPortfolioArtista(this.idUser).subscribe(
+        (response: Portfolio) => {
+          console.log(response);
+          this.portfolioImages = response as Portfolio[];
+        },
+        (err) => {
+          throw err;
+        }
+      );
   }
-
 }
--- src/app/perfil/component/perfil.component.ts
+++ src/app/perfil/component/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Perfil } from '../service/perfil';
 import { PerfilService } from '../service/perfil.service';
+import { UserPortfolio } from '../service/UserPortfolio';
 
 @Component({
   selector: 'app-perfil',
@@ -10,6 +11,18 @@ import { PerfilService } from '../service/perfil.service';
 export class PerfilComponent implements OnInit {
 
   profile!: Perfil;
+  userPortfolioImages: UserPortfolio[] = [];
+  selectedPortfolioContent!: FileList;
+
+  portfolioContent: any = {
+    publicationID: null,
+    portfolioImageUrl: null,
+    description: null
+  };
+
+  portfolioDescription: any = {
+    description: null,
+  };
 
   constructor(
     public perfilService: PerfilService,
@@ -24,7 +37,50 @@ export class PerfilComponent implements OnInit {
       (err) => {
         throw err;
       }
-    )
+    ),
+      this.perfilService.getUserPortfolio().subscribe(
+        (response: UserPortfolio) => {
+          console.log(response);
+          this.userPortfolioImages = response as UserPortfolio[];
+        },
+        (err) => {
+          throw err;
+        }
+      );
   }
 
+  updateDescription() {
+    this.perfilService
+      .updatePortfolioDescription(this.portfolioContent.publicationID, this.portfolioDescription.description)
+      .subscribe(
+        (response) => {
+          return response;
+        },
+        (err) => {
+          throw err;
+        }
+      );
+  }
+
+  async insertPortfolioFile() {
+    const file = this.selectedPortfolioContent.item(0);
+    this.portfolioContent.portfolioImageUrl = await this.perfilService.uploadPortfolioFile(file!);
+    this.perfilService
+      .insertPortfolioContent(
+        this.portfolioContent.portfolioImageUrl,
+        this.portfolioContent.description
+      )
+      .subscribe(
+        (response) => {
+          return response;
+        },
+        (err) => {
+          throw err;
+        }
+      );
+  }
+
+  selectPortfolioFile(event: any) {
+    this.selectedPortfolioContent = event.target.files;
+  }
 }
