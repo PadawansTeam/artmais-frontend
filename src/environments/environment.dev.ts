f36d1c04e98716d7c78b94c8f305a78e0a823333
João Victor Teixeira De Oliveira <joliveira@acesso.com>
2021-10-03 17:36:54 -0300

    Remove aws credentials

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: false,
   apiURL: 'https://localhost:5001/',
-  accessKeyId: 'AKIA3HSZCE6ZIDLOOE5F',
-  secretAccessKey: '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',
-  bucket: 'bucket-artmais',
 };
