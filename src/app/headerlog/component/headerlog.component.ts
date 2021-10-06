8fe5dd6e4c7ad07f301c053a6eb7fed432be3b9d
Gabriela Viana Bilinski <gabriela.bilinski@bilinski.com.br>
2021-10-06 20:30:07 -0300

    feat(pesquisa): added search component

--- src/app/headerlog/component/headerlog.component.ts
+++ src/app/headerlog/component/headerlog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosService } from '../../usuarios/service/usuarios.service';
 
 @Component({
   selector: 'app-headerlog',
@@ -8,12 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderlogComponent implements OnInit {
   className!: string;
   mobile: boolean = false;
+  search: String | undefined;
 
-  constructor() { }
+  constructor(
+    public usuariosService: UsuariosService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     if (window.screen.width < 768) { 
       this.mobile = true;
+    }
   }
+
+  goSearch() {
+    var x = window.location.href.split("/");
+    if(x[x.length-2] == "pesquisa"){
+      if(this.search === x[x.length-1]){
+        window.location.reload();
+      }else{
+        this.router.navigate(['/pesquisa', this.search]);
+        setInterval(()=>{
+          window.location.reload();
+        }, 500)
+      }
+    }else{
+      this.router.navigate(['/pesquisa', this.search]);
+    }
   }
 }
