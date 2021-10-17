a3c9b085d85f434a8e93980158d4f3f5eec66342
Bruna Rossini <brunarossini94@gmail.com>
2021-10-17 13:45:36 -0300

    Alterações para teste usuario comum

--- src/app/teste/service/recommendation.service.ts
+++ src/app/teste/service/recommendation.service.ts
@@ -1,38 +1,34 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import * as AWS from 'aws-sdk/global';
-import * as S3 from 'aws-sdk/clients/s3';
 
-@Injectable({  
-  providedIn: 'root',
-})
-export class TesteService {
-
+@Injectable()
+export class RecommendationService {
   public artPlusURL = `${environment.apiURL}`;
-  public token = localStorage.getItem('token');
-
-  PortfolioFolder = 'portfolio-pictures/';
-  DateTimeNow = new Date();
+  public token = localStorage.getItem('token')
 
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${this.token}`,
+      'Authorization': `Bearer ${this.token}`
     }),
   };
 
-  customHttpOptions = {
-    headers: new HttpHeaders({
-      Authorization: `Bearer ${this.token}`,
-    }),
-  };
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
-  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    if (this.token == undefined || this.token == null) {
+      this.router.navigate(['']);
+    }
+  }
 
-  getUser(): Observable<any> {
-    return this.http.get(this.artPlusURL + 'v1/User', this.httpOptions);
+  getValidation(): Observable<any> {
+    return this.http.post(this.artPlusURL + 'v1/Validation', {}, this.httpOptions);
   }
 
   getRecommendations(): Observable<any> {
@@ -49,8 +45,3 @@ export class TesteService {
     return this.http.get(this.artPlusURL + 'v1/Recomendation/users', this.httpOptions)
   }
 }
-
-
-
-  
-
