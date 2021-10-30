075cc9a604f4a5fe30425aa86565259d27388235
Bruna Rossini <brunarossini94@gmail.com>
2021-10-30 14:53:40 -0300

    teste9

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      defaultSrc: ["'self'","https:","data:","artmais-frontend.herokuapp.com","https://artmais-frontend.herokuapp.com/"],
-      scriptSrc: ["'self'","*","https://static.ads-twitter.com","https://www.google-analytics.com","'sha256-q2sY7jlDS4SrxBg6oq/NBYk9XVSwDsterXWpH99SAn0='"],
-      imgSrc: ["'self'","*","data:","https://s3.amazonaws.com","https://twitter.com https://pbs.twimg.com"],
-      fontSrc: ["'self","*","https://fonts.gstatic.com"],
-      styleSrc: ["'self'","*","https://fonts.googleapis.com"],
-      frameAncestors: ["'none'"],
+      defaultSrc: ["'self'"],
+      fontSrc: ["*"],
+      imgSrc: ["*","data:"],
+      scriptSrc: ["*"],   
+      styleSrc: ["*"],
     },
   })
 );
