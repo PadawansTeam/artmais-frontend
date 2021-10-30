9be01e5a17b1f73e6f75beec044aba2246340de3
Bruna Rossini <brunarossini94@gmail.com>
2021-10-30 11:45:11 -0300

    teste9

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(
   helmet.contentSecurityPolicy({
     defaultSrc: ["'self'"],
     scriptSrc: ["https://static.ads-twitter.com","https://www.google-analytics.com","'sha256-q2sY7jlDS4SrxBg6oq/NBYk9XVSwDsterXWpH99SAn0='"],
+    imgSrc: ["'self'","https://s3.amazonaws.com","https://twitter.com"," https://pbs.twimg.com"],
     reportOnly: false,
     })
 );
