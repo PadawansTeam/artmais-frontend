4f97a6994917e85704df930c3b4b4832e8ecaac4
Bruna Rossini <brunarossini94@gmail.com>
2021-10-30 03:38:24 -0300

    teste6

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 //Header secure
 const referrerPolicy = require('referrer-policy');
 const helmet = require('helmet');
-app.use(referrerPolicy({ policy: 'no-referrer'}));
+app.use(referrerPolicy({ policy: 'same-origin'}));
 //app.use(referrerPolicy({ policy: 'strict-origin-when-cross-origin'}));
 app.use(helmet.noSniff());
 app.use(helmet.frameguard());
