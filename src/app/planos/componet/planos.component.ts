2937d29fceefbe70b36373ec0fb48a007dbf91c1
Bruna Rossini <brunarossini94@gmail.com>
2021-11-11 19:30:53 -0300

    Pagina+integração planos/assinantePremium

--- src/app/planos/componet/planos.component.ts
+++ src/app/planos/componet/planos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Assinante } from 'src/app/assinante/service/assinante';
+import { AssinanteService } from 'src/app/assinante/service/assinante.service';
+import { RecommendationService } from 'src/app/homepage/service/recommendation.service';
 import { PlanosService } from '../service/planos.service';
 
 @Component({
@@ -8,24 +11,59 @@ import { PlanosService } from '../service/planos.service';
   styleUrls: ['./planos.component.css']
 })
 export class PlanosComponent implements OnInit {
+  roleUser: boolean = false;
+  isPremium!: Assinante;
+  ifPremium: boolean = false;
 
   constructor(
     private planosService: PlanosService,
-    private router: Router
+    private router: Router,
+    public assinanteService: AssinanteService,
+    public recommendationService: RecommendationService,
+
   ) { }
 
   ngOnInit(): void {
     this.planosService.ngOnInit();
+    this.roleIfClient();
+    this.AssinanteIfPremium();
     this.planosService.getValidation().subscribe(
-      (response) => {}, 
+      (response) => { },
       (err) => {
         if (err.status == 401) {
           this.router.navigate(['']);
-        }else if(err.status == 500){
+        } else if (err.status == 500) {
           this.router.navigate(['/erro']);
         }
       }
     );
   }
+
+  roleIfClient() {
+    this.recommendationService.getRole().subscribe(
+      (response) => {
+        console.warn('response Client', response.role);
+        if (response.role === 'Client') {
+          this.roleUser = true;
+        } else {
+          this.roleUser = false;
+        }
+      }
+    );
+  }
+
+  AssinanteIfPremium() {
+    this.assinanteService.getAssinaturaPremium().subscribe(
+      (response) => {
+        console.warn('response artista premium', response);
+        if (response.isPremium === true) {
+          this.ifPremium = true;
+        } else {
+          this.ifPremium = false;
+        }
+      }
+    )
+  }
+
 
 }
