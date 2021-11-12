b3ae16575673be70ff17138c9767057482fdcaf2
joao.teixeira <joao.teixeira@checkok.com.br>
2021-11-12 14:09:30 -0300

    Fix router import

--- src/app/pagamentoplanos/service/pagamentoplanos.service.ts
+++ src/app/pagamentoplanos/service/pagamentoplanos.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from 'express';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -10,19 +10,19 @@ import { environment } from 'src/environments/environment';
 export class PagamentoplanosService {
   public artPlusURL = `${environment.apiURL}`;
   public token = localStorage.getItem('token');
-  
+
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       Authorization: `Bearer ${this.token}`,
     }),
   };
-  
+
   constructor(
     private http: HttpClient,
     private router: Router
   ) { }
-  
+
   insertPagamento(): Observable<any> {
     return this.http.post(`${this.artPlusURL}` + `v1`, this.httpOptions);
   }
