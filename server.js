70443a45c4519f515e967c68667b3a474b564adb
Bruna Rossini <brunarossini94@gmail.com>
2021-11-18 08:26:20 -0300

    teste csp03

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(
     useDefaults: false,
     directives: {
       "default-src": helmet.contentSecurityPolicy.dangerouslyDisableDefaultSrc,
-      "scriptSrc": ["'self'","https://static.ads-twitter.com","https://www.google-analytics.com","'sha256-q2sY7jlDS4SrxBg6oq/NBYk9XVSwDsterXWpH99SAn0='","https://boxicons.com/","https://undraw.co/illustrations",, (req, res) => `'nonce-${res.locals.cspNonce}'`],
-      "style-src": ["'self'","https://fonts.googleapis.com","https://boxicons.com/","https://undraw.co/illustrations"],
+      "scriptSrc": ["'self'", "https://static.ads-twitter.com", "https://www.google-analytics.com", "'sha256-q2sY7jlDS4SrxBg6oq/NBYk9XVSwDsterXWpH99SAn0='", "https://boxicons.com/", "https://undraw.co/illustrations", (req, res) => `'nonce-${res.locals.cspNonce}'`],
+      "style-src": ["'self'", "https://fonts.googleapis.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
     },
   })
 );
