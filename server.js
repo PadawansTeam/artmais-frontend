2b8a38c1339a384453a5fde1e457ed4b6a3041e3
Bruna Rossini <brunarossini94@gmail.com>
2021-11-18 10:23:23 -0300

    teste csp03

--- server.js
+++ server.js
@@ -6,49 +6,42 @@ const app = express();
 const helmet = require('helmet');
 const csp = require('content-security-policy');
 const cspPolicy = {
-  'default-src': [csp.SRC_ANY,csp.SRC_SELF,'self'],
+  'default-src': 'self',
 
   'style-src': [
-      csp.SRC_SELF,
-      csp.SRC_USAFE_INLINE,
-      'https://fonts.googleapis.com/',
-      'https://use.typekit.net',
-      'https://fonts.googleapis.com',
-      'https://boxicons.com/',
-      'https://undraw.co/illustrations',
-      'https://static.ads-twitter.com',
-      'self'
+    'https://fonts.googleapis.com/',
+    'https://use.typekit.net',
+    'https://fonts.googleapis.com',
+    'https://boxicons.com/',
+    'https://undraw.co/illustrations',
+    'https://static.ads-twitter.com',
+    'self'
 
 
   ],
   'script-src': [
-      csp.SRC_SELF,
-      csp.SRC_USAFE_INLINE,
-      csp.SRC_UNSAFE_EVAL,
-      'https://fonts.googleapis.com/',
-      'http://apis.google.com/',
-      'http://connect.facebook.net/',
-      '*.facebook.com',
-      'self'
+    'https://fonts.googleapis.com/',
+    'http://apis.google.com/',
+    'http://connect.facebook.net/',
+    '*.facebook.com',
+    'self'
   ],
-  'connect-src': csp.SRC_ANY,
+  'connect-src': ['*', 'self'],
   'child-src': [
-      csp.SRC_SELF,
-      'https://apis.google.com',
-      'https://facebook.com',
-      'https://www.googleapis.com/',
-      'https://accounts.google.com/'
+    'https://apis.google.com',
+    'https://facebook.com',
+    'https://www.googleapis.com/',
+    'https://accounts.google.com/',
+    'self'
   ],
   'img-src': [
-      csp.SRC_ANY,
-      csp.SRC_DATA,
-      'data:"',
-      'https://s3.amazonaws.com',
-      'https://twitter.com',
-      'https://pbs.twimg.com',
-      'https://boxicons.com/',
-      'https://undraw.co/illustrations'
-
+    'data:"',
+    'https://s3.amazonaws.com',
+    'https://twitter.com',
+    'https://pbs.twimg.com',
+    'https://boxicons.com/',
+    'https://undraw.co/illustrations',
+    'self'
   ]
 };
 const globalCSP = csp.getCSP(cspPolicy);
