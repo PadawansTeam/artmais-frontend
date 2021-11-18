68c272c7336dc5c8ce13d45dfde9af55a99a719a
Bruna Rossini <brunarossini94@gmail.com>
2021-11-18 10:36:02 -0300

    teste csp03

--- server.js
+++ server.js
@@ -4,53 +4,16 @@ const path = require("path");
 const app = express();
 //Header secure
 const helmet = require('helmet');
-const csp = require('content-security-policy');
-const cspPolicy = {
-  'default-src': 'self',
-
-  'style-src': [
-    'https://fonts.googleapis.com/',
-    'https://use.typekit.net',
-    'https://fonts.googleapis.com',
-    'https://boxicons.com/',
-    'https://undraw.co/illustrations',
-    'https://static.ads-twitter.com',
-    'self'
-
-
-  ],
-  'script-src': [
-    'https://fonts.googleapis.com/',
-    'http://apis.google.com/',
-    'http://connect.facebook.net/',
-    '*.facebook.com',
-    'self'
-  ],
-  'connect-src': ['*', 'self'],
-  'child-src': [
-    'https://apis.google.com',
-    'https://facebook.com',
-    'https://www.googleapis.com/',
-    'https://accounts.google.com/',
-    'self'
-  ],
-  'img-src': [
-    'data:"',
-    'https://s3.amazonaws.com',
-    'https://twitter.com',
-    'https://pbs.twimg.com',
-    'https://boxicons.com/',
-    'https://undraw.co/illustrations',
-    'self'
-  ]
-};
-const globalCSP = csp.getCSP(cspPolicy);
-app.use(globalCSP);
 app.use(helmet.hsts());
 app.use(helmet.referrerPolicy());
 app.use(helmet.noSniff());
 app.use(helmet.frameguard());
 app.use(helmet.xssFilter());
+app.use(
+  helmet({
+    contentSecurityPolicy: true,
+  })
+);
 app.use(
   helmet.referrerPolicy({
     policy: ["no-referrer", "strict-origin-when-cross-origin"],
