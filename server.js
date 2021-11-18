18d7caf1406d9eca33bd0839afc9b9f827292df9
Bruna Rossini <brunarossini94@gmail.com>
2021-11-18 07:44:49 -0300

    teste csp03

--- server.js
+++ server.js
@@ -6,19 +6,18 @@ const app = express();
 const helmet = require('helmet');
 app.use(helmet.hsts());
 app.use(helmet.referrerPolicy());
-app.use((req, res, next) => {
-  res.locals.cspNonce = crypto.randomBytes(16).toString("hex");
-  next();
-});
 app.use(
   helmet.contentSecurityPolicy({
     useDefaults: false,
     directives: {
-      defaultSrc: ["'self'", "*", "https:", "data:", "artmais-frontend.herokuapp.com", "https://artmais-frontend.herokuapp.com/", "artmais-backend.herokuapp.com", "https://artmais-backend.herokuapp.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
-      scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.cspNonce}'`, "'unsafe-inline'", "'unsafe-eval'"],
-      styleSrc: ["'self'", "'unsafe-inline'", "*", "'unsafe-eval'", "https://fonts.googleapis.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
-      imgSrc: ["'self'", "*", "data:", "https://s3.amazonaws.com", "https://twitter.com", "https://pbs.twimg.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
-      fontSrc: ["'self'", "*", "https://fonts.gstatic.com", "https://boxicons.com/", "https://undraw.co/illustrations"],   
+      "default-src": ["*"],
+      "script-src": ["*"],
+      "style-src": ["*"],
+      "base-uri": ["*"],
+      "form-action": ["*"],
+      "manifest-src": ["*"],
+      "media-src": ["*"],
+      "object-src": ["*"],
     },
   })
 );
