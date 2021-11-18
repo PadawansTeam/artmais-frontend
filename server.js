5671d5999585098e45ca64673531ad9dd64899cc
Bruna Rossini <brunarossini94@gmail.com>
2021-11-18 09:44:48 -0300

    teste csp03

--- server.js
+++ server.js
@@ -3,20 +3,55 @@ const express = require("express");
 const path = require("path");
 const app = express();
 //Header secure
-const permissionsPolicy = require('permissions-policy');
 const helmet = require('helmet');
+const csp = require('content-security-policy');
+const cspPolicy = {
+  'default-src': csp.SRC_ANY,
+  'style-src': [
+      csp.SRC_SELF,
+      csp.SRC_USAFE_INLINE,
+      'https://fonts.googleapis.com/',
+      'https://use.typekit.net',
+      'https://fonts.googleapis.com',
+      'https://boxicons.com/',
+      'https://undraw.co/illustrations',
+      'https://static.ads-twitter.com',
+      'self'
+
+
+  ],
+  'script-src': [
+      csp.SRC_SELF,
+      csp.SRC_USAFE_INLINE,
+      csp.SRC_UNSAFE_EVAL,
+      'https://fonts.googleapis.com/',
+      'http://apis.google.com/',
+      'http://connect.facebook.net/',
+      '*.facebook.com',
+      'self'
+  ],
+  'connect-src': csp.SRC_ANY,
+  'child-src': [
+      csp.SRC_SELF,
+      'https://apis.google.com',
+      'https://facebook.com',
+      'https://www.googleapis.com/',
+      'https://accounts.google.com/'
+  ],
+  'img-src': [
+      csp.SRC_ANY,
+      csp.SRC_DATA,
+      'data:"',
+      'https://s3.amazonaws.com',
+      'https://twitter.com',
+      'https://pbs.twimg.com',
+      'https://boxicons.com/',
+      'https://undraw.co/illustrations'
+
+  ]
+};
 app.use(helmet.hsts());
 app.use(helmet.referrerPolicy());
-app.use(permissionsPolicy({
-  features: {
-      fullscreen: ['self']
-  }
-}));
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
 app.use(helmet.noSniff());
 app.use(helmet.frameguard());
 app.use(helmet.xssFilter());
