9f23c32b5e1fc4e2c0b78e3cc6d3ef675f28c19b
Bruna Rossini <brunarossini94@gmail.com>
2021-11-18 07:30:54 -0300

    teste csp03

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: false,
     directives: {
-      defaultSrc: ["'self'", , "*", "https:", "data:", "artmais-frontend.herokuapp.com", "https://artmais-frontend.herokuapp.com/", "artmais-backend.herokuapp.com", "https://artmais-backend.herokuapp.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
+      defaultSrc: ["'self'", "*", "https:", "data:", "artmais-frontend.herokuapp.com", "https://artmais-frontend.herokuapp.com/", "artmais-backend.herokuapp.com", "https://artmais-backend.herokuapp.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
       scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.cspNonce}'`, "'unsafe-inline'", "'unsafe-eval'"],
       styleSrc: ["'self'", "'unsafe-inline'", "*", "'unsafe-eval'", "https://fonts.googleapis.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
       imgSrc: ["'self'", "*", "data:", "https://s3.amazonaws.com", "https://twitter.com", "https://pbs.twimg.com", "https://boxicons.com/", "https://undraw.co/illustrations"],
