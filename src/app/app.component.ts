e4de43d9815b6fbe71df9e69acefc54d452a415c
Bruna Rossini <brunarossini94@gmail.com>
2021-12-07 19:46:54 -0300

    remove redirect http-https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
